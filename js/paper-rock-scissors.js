bb7388006db704873df3cd92ad7397acb7e6cb28
AggieWB <usmaggie23@gmail.com>
2020-05-15 12:49:46 -0700

    Enclose js file in an IIFE for security to user manipulation

--- js/paper-rock-scissors.js
+++ js/paper-rock-scissors.js
@@ -1,36 +1,38 @@
-document.getElementById("paper").addEventListener("click", function(){
-    chooseComputerMove("paper");
-}); 
-document.getElementById("rock").addEventListener("click", function(){
-    chooseComputerMove("rock");
-});
-document.getElementById("scissors").addEventListener("click", function(){
-    chooseComputerMove("scissors");
-}); 
+(function (){
+    document.getElementById("paper").addEventListener("click", function(){
+        chooseComputerMove("paper");
+    }); 
+    document.getElementById("rock").addEventListener("click", function(){
+        chooseComputerMove("rock");
+    });
+    document.getElementById("scissors").addEventListener("click", function(){
+        chooseComputerMove("scissors");
+    }); 
 
-function playRound(computerMove, userMove){
-    console.log(computerMove + " " + userMove);
-    if(computerMove === "rock" && userMove === "scissors"){
-        document.getElementById("win").innerHTML = "Computer: Rock vs. You: Scissors.<br>The computer wins!";
-    } else if(computerMove === "scissors" && userMove === "paper"){
-        document.getElementById("win").innerHTML = "Computer: Scissors vs. You: Paper.<br>The computer wins!";
-    } else if(computerMove === "paper" && userMove === "rock"){
-        document.getElementById("win").innerHTML = "Computer: Paper vs. You: Rock.<br>The computer wins!";
-    } else if(computerMove === userMove){
-        computerMove = computerMove.charAt(0).toUpperCase() + computerMove.slice(1);
-        document.getElementById("win").innerHTML = "Computer and You: " + computerMove + ".<br> TIE!";
-    } else {
-        computerMove = computerMove.charAt(0).toUpperCase() + computerMove.slice(1);
-        userMove = userMove.charAt(0).toUpperCase() + userMove.slice(1);
-        document.getElementById("win").innerHTML = "Computer: " + computerMove + " vs. You: " + userMove + ".<br>You win!";
+    function playRound(computerMove, userMove){
+        console.log(computerMove + " " + userMove);
+        if(computerMove === "rock" && userMove === "scissors"){
+            document.getElementById("win").innerHTML = "Computer: Rock vs. You: Scissors.<br>The computer wins!";
+        } else if(computerMove === "scissors" && userMove === "paper"){
+            document.getElementById("win").innerHTML = "Computer: Scissors vs. You: Paper.<br>The computer wins!";
+        } else if(computerMove === "paper" && userMove === "rock"){
+            document.getElementById("win").innerHTML = "Computer: Paper vs. You: Rock.<br>The computer wins!";
+        } else if(computerMove === userMove){
+            computerMove = computerMove.charAt(0).toUpperCase() + computerMove.slice(1);
+            document.getElementById("win").innerHTML = "Computer and You: " + computerMove + ".<br> TIE!";
+        } else {
+            computerMove = computerMove.charAt(0).toUpperCase() + computerMove.slice(1);
+            userMove = userMove.charAt(0).toUpperCase() + userMove.slice(1);
+            document.getElementById("win").innerHTML = "Computer: " + computerMove + " vs. You: " + userMove + ".<br>You win!";
+        }
     }
-}
 
-function chooseComputerMove(userMove){
-    var moves = ["paper", "rock", "scissors"];
-    computerMove = moves[Math.floor((Math.random() * 3))];
-    playRound(computerMove, userMove);
-}
+    function chooseComputerMove(userMove){
+        var moves = ["paper", "rock", "scissors"];
+        computerMove = moves[Math.floor((Math.random() * 3))];
+        playRound(computerMove, userMove);
+    }
 
-var year = new Date().getFullYear();
-document.querySelector("footer").innerHTML = "Aggie Wheeler Bateman &copy; " +  year + " <a href=\"mailto:aggiewbdev\">aggiewbdev@gmail.com</a>";
+    var year = new Date().getFullYear();
+    document.querySelector("footer").innerHTML = "Aggie Wheeler Bateman &copy; " +  year + " <a href=\"mailto:aggiewbdev\">aggiewbdev@gmail.com</a>";
+}());
